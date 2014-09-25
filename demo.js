8883e9d9494307b33fd971891b6faa335ec5f176
jian.chen <jian05.chen>
2014-09-25 10:33:13 +0800

    u

--- demo.js
+++ demo.js
@@ -90,7 +90,7 @@
               var x = (e.clientX || e.touches[0].x) - (offset.x || offset.left),
                   y = (e.clientY || e.touches[0].y) - (offset.y || offset.top),
                   cy = demo.currentY;
-              helper.write(e.touches[0]);
+              helper.write(e);
               demo.isWait = false;
               if (Math.abs(that.width / 2 - x) < 40 && (Math.abs(cy - y) < 20 || demo.isHit)) {
                 demo.isHit = true;
